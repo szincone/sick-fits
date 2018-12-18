e48ffdcfd2acb3c659488bca1d390a9f99968800
szincone <sawyerzincone@gmail.com>
2018-12-18 09:39:32 -0700

    created app component - wanted to override default app behavior

---
+++ frontend/pages/_app.js
@@ -0,0 +1,14 @@
+import App, { Container } from "next/app";
+
+class MyApp extends App {
+  render() {
+    const { Component } = this.props;
+    return (
+      <Container>
+        <Component />
+      </Container>
+    );
+  }
+}
+
+export default MyApp;
