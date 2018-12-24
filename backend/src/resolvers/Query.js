dc5e33b1cbea368074fb90223dac4b3b9fe1f409
szincone <sawyerzincone@gmail.com>
2018-12-23 23:25:11 -0700

    created get items query in graphql resolver

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,14 @@
-const Query = {};
+const { forwardTo } = require("prisma-binding");
+
+const Query = {
+  items: forwardTo("db"),
+
+  // this is a custom resolver you wrote but since it does the same thing
+  // as the default one already in prisma, you can just use the prisma one
+  // async items(parent, args, ctx, info) {
+  //   const items = await ctx.db.query.items();
+  //   return items;
+  // },
+};
 
 module.exports = Query;
