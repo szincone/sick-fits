5c8aed6885c9e8223ec8fd011e77f05f0c45369c
szincone <sawyerzincone@gmail.com>
2019-01-01 17:25:53 -0700

    added apollo boilerplate, updated dependencies

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,15 +1,30 @@
 import App, { Container } from "next/app";
 import Page from "../components/Page";
+import { ApolloProvider } from "react-apollo";
+import withData from "../lib/withData";
 
-export default class MyApp extends App {
+class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+    // this exposes the query to the user
+    pageProps.query = ctx.query;
+    return { pageProps };
+  }
   render() {
-    const { Component } = this.props;
+    const { Component, apollo, pageProps } = this.props;
     return (
       <Container>
-        <Page>
-          <Component />
-        </Page>
+        <ApolloProvider client={this.props.apollo}>
+          <Page>
+            <Component {...pageProps} />
+          </Page>
+        </ApolloProvider>
       </Container>
     );
   }
 }
+
+export default withData(MyApp);
