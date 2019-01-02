20426f0a0b2f3336fe873029de56feaf5b0c827b
szincone <sawyerzincone@gmail.com>
2019-01-01 18:34:55 -0700

    created basic graphql query - gets items for main and shop pages

--- frontend/components/Items.js
+++ frontend/components/Items.js
@@ -1,12 +1,33 @@
 import React, { Component } from "react";
 import { Query } from "react-apollo";
+import gql from "graphql-tag";
 
-export default class Items extends Component {
+const ALL_ITEMS_QUERY = gql`
+  query ALL_ITEMS_QUERY {
+    items {
+      id
+      title
+      price
+      description
+      image
+      largeImage
+    }
+  }
+`;
+class Items extends Component {
   render() {
     return (
       <div>
         <p>Items</p>
+        <Query query={ALL_ITEMS_QUERY}>
+          {payload => {
+            console.log(payload);
+            return <p>Child of the query</p>;
+          }}
+        </Query>
       </div>
     );
   }
 }
+
+export default Items;
